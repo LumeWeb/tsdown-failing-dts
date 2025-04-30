3ba7eca9022e1506f7d8a5a39e78184e596adbec
Derrick Hammer <derrick@derrickhammer.com>
2025-04-30 14:22:40 -0400

    reprod: github.com/rolldown/tsdown/issues/183

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,38 +1,52 @@
-import type { Options } from "tsdown";
+import { defineConfig, type Options } from "tsdown";
 
-import { defineConfig } from "tsdown";
-
-const configs: Options[] = [
-  // Config 1: CJS Output (Main code)
-  {
-    clean: true,
-    entry: ["src/**/*", "!**/image*/**", "!**/*image*/**"],
-    external: ["fs", "path", "react", "react-dom"],
-    format: ["cjs"],
+const commonOptions: Omit<
+    Options,
+    | "clean"
+    | "dts"
+    | "entry"
+    | "format"
+    | "outDir"
+    | "outExtensions"
+    | "outputOptions"
+> = {
     minify: false,
-    outDir: "dist/cjs",
-    //  outExtensions: () => ({ js: ".cjs" }),
+    platform: "node",
     sourcemap: true,
-    target: "esnext",
     tsconfig: "./tsconfig.json",
-  },
+};
 
-  // Config 2: ESM Output (Main code + DTS for main code)
-  {
-    clean: true,
-    dts: true,
-    entry: ["src/**/*", "!**/image*/**", "!**/*image*/**"],
-    external: ["fs", "path", "react", "react-dom"],
-    format: ["esm"],
-    minify: false,
-    outDir: "dist/esm",
-    outputOptions: {
-      chunkFileNames: "[name].js",
+const commonOutputOptions = {
+    chunkFileNames: "[name].js",
+    entryFileNames: "[name].js",
+};
+
+export default defineConfig([
+    {
+        ...commonOptions,
+        clean: true,
+        dts: false,
+        entry: ["src"],
+        format: "cjs",
+        outDir: "dist/cjs",
+        outExtensions: () => ({ js: ".cjs" }),
+        outputOptions: {
+            ...commonOutputOptions,
+            entryFileNames: "[name].js",
+            exports: "named",
+        },
     },
-    sourcemap: true,
-    target: "esnext",
-    tsconfig: "./tsconfig.json",
-  },
-];
-
-export default defineConfig(configs);
+    {
+        ...commonOptions,
+        clean: true,
+        dts: { resolve: true },
+        entry: ["src"],
+        format: "esm",
+        outDir: "dist/esm",
+        outputOptions: {
+            ...commonOutputOptions,
+            entryFileNames: "[name].js",
+        },
+        target: "esnext",
+    },
+]);
